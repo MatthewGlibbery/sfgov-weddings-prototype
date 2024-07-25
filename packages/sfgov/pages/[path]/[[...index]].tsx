744f8fbf4ed52deabbe9fd5f3394442412c3cc2b
ant <anthony.kong@sfgov.org>
2024-07-24 17:17:56 -0700

    chore/logs (#164)
    chore: some silly debug logging to inspect fetch options

--- packages/sfgov/pages/[path]/[[...index]].tsx
+++ packages/sfgov/pages/[path]/[[...index]].tsx
@@ -50,9 +50,10 @@ export const getServerSideProps: GetServerSideProps = async ({
     }
   } catch (error) {
     console.error(
-      'No page found for path: "%s", locale: "%s"',
+      'No page found for path: "%s", locale: "%s", query: "%s"',
       resolvedUrl,
-      locale
+      locale,
+      query
     )
   }
   return {
