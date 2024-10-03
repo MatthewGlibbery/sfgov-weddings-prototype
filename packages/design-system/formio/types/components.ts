3524ccb7543005c330533fe097acbb8138a645a6
Shawn Allen <shawnbot@users.noreply.github.com>
2024-10-03 10:34:11 -0700

    fix(build): move all required build deps into production (#192)
    * chore(deps): upgrade next, i18next, etc.
    
    * chore(deps): pin sfgov dependency versions
    
    * chore(deps): nix tailwindcss from sfgov, move react-error-boundary to prod
    
    * chore(deps): upgrade design-system deps, demote formiojs to dev
    
    * chore(deps): move i18next backends to sfgov; upgrade i18next-parser
    
    * chore(deps): upgrade tslib; pin @types/react*
    
    * fix(build): move all required build deps into production
    
    * fix: fix id selectors in TableOfContents
    
    * chore: fix choices.js options type
    
    * chore: swc in package-lock
    
    * chore: bump next-env.d.ts
    
    * fix: workaround for npm/cli#4828

--- packages/design-system/formio/types/components.ts
+++ packages/design-system/formio/types/components.ts
@@ -3,7 +3,7 @@ import type {
   ValidateOptions
 } from 'formiojs'
 import type { Override } from './utils'
-import type { Options as ChoicesOptions } from 'choices.js/src/scripts/interfaces/options'
+import type { Choices } from 'choices.js'
 
 /**
  * This is our "abstract" component schema, which does not specify an explict
@@ -310,7 +310,7 @@ export type SelectSchema = TypedComponentSchema<
     template?: string
     lazyLoad?: boolean
     // https://github.com/formio/formio.js/blob/v4.21.3/src/components/select/Select.js#L898-L939
-    customOptions?: Partial<ChoicesOptions>
+    customOptions?: Partial<Choices.Options>
   } & (
     | {
         dataSrc?: 'values'
