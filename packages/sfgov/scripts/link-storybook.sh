cd9000b2247ad71a0bea2fe4af2693a44cac9d49
Shawn Allen <shawnbot@users.noreply.github.com>
2024-10-04 15:02:11 -0700

    fix(deps): storybook and design-system production dependencies (#197)
    * fix(deps): move storybook build deps to prod
    
    * fix(deps): remove duplicate @thoughtbot/tailwindcss-aria-attributes
    
    * fix(build): always build storybook in site prebuild
    
    * chore: make sure sfgov/public exists in link-storybook.sh

--- packages/sfgov/scripts/link-storybook.sh
+++ packages/sfgov/scripts/link-storybook.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 out=public/storybook
-static_dir=storybook-static
 
 # delete it if it's a directory
 [[ -d $out ]] && rm -rf $out
 
 # go up into the storybook package and build the static dir if doesn't exist
-cd ../storybook
-if [[ ! -d $static_dir ]]; then
-    npm run build
-fi
-cd -
+pushd ../storybook
+npm run build
+popd
 
-# this needs to reach up from the public dir, hence the ../..
-ln -sf ../../storybook/$static_dir $out
+# symlink the storybook build directory to public/storybook
+ln -sf ../storybook/storybook-static $out
