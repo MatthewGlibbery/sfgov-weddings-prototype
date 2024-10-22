4eb4749bdad5195394de358a3be75aea6b9b30bd
Shawn Allen <shawnbot@users.noreply.github.com>
2024-10-22 14:23:54 -0700

    fix: image URLs (#217)
    * chore: comments
    
    * fix: don't requireEnv('NEXT_PUBLIC_IMAGE_DOMAIN')

--- packages/sfgov/lib/env.ts
+++ packages/sfgov/lib/env.ts
@@ -4,6 +4,9 @@ export function getenv(name: string) {
   return process.env[name]
 }
 
+/**
+ * Get a key of process.env and throw an error if the value is undefined.
+ */
 export function requireEnv(name: string) {
   const value = process.env[name]
   if (value === undefined) {
--- packages/sfgov/lib/imageLoader.ts
+++ packages/sfgov/lib/imageLoader.ts
@@ -1,19 +1,31 @@
 /* istanbul ignore file */
 'use client'
+import type { ImageLoaderProps } from 'next/image'
+import { coalesce, getenv } from './env'
 
-import { requireEnv } from './env'
-
-type ImageLoaderProps = {
-  src: string
-  width?: number
-  quality?: number
-}
-
-/* TODO: We might want to load/leverage Wagtail renditions, but
- * first we should understand the business case, e.g. when to
- * load a thumbnail rather than the original image
+/**
+ * This file isn't imported directly; it's referenced in our next config's
+ * `images.loaderFile` option and imported by next/image. The default export
+ * needs to implement the API:
+ *
+ * (props: ImageLoaderProps) => string
+ *
+ * @see https://nextjs.org/docs/app/api-reference/components/image#loaderfile
+ *
+ * We can do lots of different things here, for example:
+ *
+ * - CloudFront image optimization
+ *   https://nextjs.org/docs/app/api-reference/next-config-js/images#aws-cloudfront
+ *
+ * - Wagtail renditions
+ *   https://docs.wagtail.org/en/latest/advanced_topics/images/renditions.html
  */
+
 export default function getImageURL({ src }: ImageLoaderProps) {
-  const baseURL = requireEnv('NEXT_PUBLIC_IMAGE_DOMAIN')
-  return new URL(src, baseURL).toString()
+  let baseURL = getenv('NEXT_PUBLIC_IMAGE_DOMAIN')
+  // if the value is just a domain, add the protocol prefix
+  if (baseURL && !new URL(baseURL).host) {
+    baseURL = `https://${baseURL}`
+  }
+  return baseURL ? new URL(src, baseURL).href : src
 }
