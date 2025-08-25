9ddbb1e081d97ce5c634e0a12d904352648c3553
ant <anthony.kong@sfgov.org>
2025-08-25 13:23:10 -0700

    CMS-1127 - remove geocoding from map component (#558)
    chore: remove google map geocoding component and dependencies, render static google map image only

--- packages/sfgov/components/Map.spec.tsx
+++ packages/sfgov/components/Map.spec.tsx
@@ -2,42 +2,6 @@ import { ImageFactory, LocationBlockFactory } from '@/lib/factories'
 import { render, screen } from '@testing-library/react'
 import { Map, type MapProps } from './Map'
 
-jest.mock('@react-google-maps/api', () => {
-  return {
-    withGoogleMap: (Component: any) => Component,
-    withScriptjs: (Component: any) => Component,
-    Marker: (props: any) => <div {...props} />,
-    GoogleMap: (props: any) => (
-      <div>
-        <div data-testid="mock-google-maps" />
-        {props.children}
-      </div>
-    ),
-    InfoWindow: (props: any) => <div {...props} />,
-    MarkerF: (props: any) => <div {...props} />,
-    useLoadScript: () => ({
-      isLoaded: true
-    })
-  }
-})
-
-jest.mock('react-geocode', () => ({
-  fromAddress: (props: any) =>
-    Promise.resolve({
-      results: [
-        {
-          geometry: {
-            location: {
-              lat: 37.759571206469374,
-              lng: -122.44429767907141
-            }
-          }
-        }
-      ]
-    }),
-  setKey: () => true
-}))
-
 describe('Map', () => {
   const props: MapProps = {
     address: LocationBlockFactory.make(),
@@ -47,8 +11,37 @@ describe('Map', () => {
   }
   it('renders a Map', () => {
     render(<Map {...props} />)
-    const el = screen.getByTestId('mock-google-maps')
+    const el = screen.getByTestId('map')
 
     expect(el).toBeInTheDocument()
+  })
+
+  it('renders a Map with an AddressTile image if an image exists', () => {
+    const props: MapProps = {
+      address: LocationBlockFactory.make(),
+      image: ImageFactory.make(),
+      locationName: '1 SVN',
+      googleMapsApiKey: 'test'
+    }
+    render(<Map {...props} />)
+    const el = screen.getByTestId('map')
+    expect(el).toBeInTheDocument()
+
+    const addressTileImg = screen.getByTestId('address-tile-image')
+    expect(addressTileImg).toBeInTheDocument()
+  })
+
+  it('renders a Map with an AddressTile without an image if there is no image', () => {
+    const props: MapProps = {
+      address: LocationBlockFactory.make(),
+      locationName: '1 SVN',
+      googleMapsApiKey: 'test'
+    }
+    render(<Map {...props} />)
+    const el = screen.getByTestId('map')
+    expect(el).toBeInTheDocument()
+
+    const addressTileImg = screen.queryByTestId('address-tile-image')
+    expect(addressTileImg).not.toBeInTheDocument()
   })
 })
--- packages/sfgov/components/Map.tsx
+++ packages/sfgov/components/Map.tsx
@@ -1,15 +1,8 @@
 import { Button, IconPhone } from '@/design-system'
 import { TypeLocationBlock, WagtailImageData } from '@/types'
-import {
-  GoogleMap,
-  MarkerF,
-  useLoadScript,
-  type Libraries
-} from '@react-google-maps/api'
 import { useTranslation } from 'next-i18next'
 import NextImage from 'next/image'
-import { ComponentType, useMemo, useState } from 'react'
-import { fromAddress, setKey } from 'react-geocode'
+import { ComponentType, useMemo } from 'react'
 import { Image, Location } from '.'
 
 export type MapProps = JSX.IntrinsicElements['div'] & {
@@ -19,8 +12,6 @@ export type MapProps = JSX.IntrinsicElements['div'] & {
   locationName: string
 }
 
-const GMAPS_LIBRARIES: Libraries = ['places']
-
 export const Map: ComponentType<MapProps> = ({
   address,
   image,
@@ -29,19 +20,8 @@ export const Map: ComponentType<MapProps> = ({
   ...rest
 }) => {
   const { t } = useTranslation()
-
-  // Sets the default coords to 1 SVN
-  const [lat, setLat] = useState(37.759571206469374)
-  const [lng, setLng] = useState(-122.44429767907141)
   const mapSize = { width: 1000, height: 400 }
   const mapZoom = 16
-
-  // istanbul ignore next
-  if (googleMapsApiKey) {
-    setKey(googleMapsApiKey)
-  } else {
-    console.error('Missing google maps API key; this will not work!')
-  }
 
   // eslint-disable-next-line max-len
   const addressQuery = useMemo(
@@ -49,44 +29,21 @@ export const Map: ComponentType<MapProps> = ({
       `${address.value.line1}, ${address.value.city} ${address.value.state}, ${address.value.zip}`,
     [address]
   )
-  fromAddress(addressQuery)
-    .then(({ results }) => {
-      setLat(results[0].geometry.location.lat)
-      setLng(results[0].geometry.location.lng)
-      return true
-    })
-    .catch(console.error)
-
-  const mapCenter = useMemo(() => ({ lat, lng }), [lat, lng])
-  const mapOptions = useMemo<google.maps.MapOptions>(
-    () => ({
-      fullscreenControl: false,
-      mapTypeControl: false,
-      scrollwheel: false,
-      streetViewControl: false,
-      zoomControlOptions: { position: 6 }
-    }),
-    []
-  )
-
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey,
-    libraries: GMAPS_LIBRARIES
-  })
 
   const AddressTile: ComponentType<{
     address: TypeLocationBlock
     image: WagtailImageData
   }> = ({ address, image }) => (
     <div className="flex flex-col md:flex-row-reverse md:gap-x-28 lg:flex-col space-y-20">
-      <div className="flex-1 aspect-[3/2]">
-        {image ? (
+      {image ? (
+        <div className="flex-1 aspect-[3/2]">
           <Image
             imageRef={image}
             className="object-cover object-center w-full"
+            data-testid="address-tile-image"
           />
-        ) : null}
-      </div>
+        </div>
+      ) : null}
       <div className="flex flex-col flex-1 space-y-20">
         <Location {...address?.value} variant="card" />
         <Button as="a" variant="tertiary" className="w-full" href="#contact">
@@ -110,40 +67,21 @@ export const Map: ComponentType<MapProps> = ({
   )
 
   return (
-    <>
-      <div className="lg:hidden">
-        <NextImage
-          src={mapImageURL.href}
-          width={mapSize.width}
-          height={mapSize.height}
-          alt={t('map-alt-text', {
-            defaultValue: 'Map of {{zyx}}',
-            zyx: locationName
-          })}
-          data-testid="map-image"
-        />
-        <div className="py-20 rounded-4">
-          <AddressTile address={address} image={image} />
-        </div>
+    <div className="relative" data-testid="map">
+      <NextImage
+        className="w-full"
+        src={mapImageURL.href}
+        width={mapSize.width}
+        height={mapSize.height}
+        alt={t('map-alt-text', {
+          defaultValue: 'Map of {{zyx}}',
+          zyx: locationName
+        })}
+        data-testid="map-image"
+      />
+      <div className="static top-28 right-60 py-20 lg:px-16 lg:absolute lg:w-[40%] lg:float-right bg-white lg:shadow-md rounded-4">
+        <AddressTile address={address} image={image} />
       </div>
-      <div className="hidden lg:block" data-testid="google-map" {...rest}>
-        {isLoaded ? (
-          <GoogleMap
-            options={mapOptions}
-            zoom={mapZoom}
-            center={{ lat, lng: lng + 0.001 }}
-            mapTypeId={google.maps.MapTypeId.ROADMAP}
-            mapContainerStyle={{ width: '100%', height: '600px' }}
-          >
-            <MarkerF position={mapCenter} />
-            <div className="w-[40%] relative top-28 right-60 py-20 px-16 float-right bg-white shadow-md rounded-4">
-              <AddressTile address={address} image={image} />
-            </div>
-          </GoogleMap>
-        ) : (
-          <div>{t('map-loading', { defaultValue: 'loading...' })}</div>
-        )}
-      </div>
-    </>
+    </div>
   )
 }
