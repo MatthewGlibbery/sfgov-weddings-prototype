600be26a6747f4651d7129a63e3f58be8f039dc4
efrysf <153565014+efrysf@users.noreply.github.com>
2026-03-24 17:34:17 -0700

    Re-add git hash to footer as data attribute

--- packages/sfgov/components/SiteFooter.tsx
+++ packages/sfgov/components/SiteFooter.tsx
@@ -41,9 +41,10 @@ const FooterColumn = classed('div', 'md:basis-1/3')
 
 export function SiteFooter(props: SiteFooterProps) {
   const { t } = useTranslation()
+  const gitHash = process.env.NEXT_PUBLIC_GIT_HASH || 'unknown'
 
   return (
-    <StyledFooter {...props} role="contentinfo">
+    <StyledFooter {...props} role="contentinfo" data-git-hash={gitHash}>
       <Container className="pt-28 md:pt-[48px]">
         <div className="grid gap-y-40 md:grid-cols-12 md:gap-28">
           <div className="md:col-span-5 content-center">
